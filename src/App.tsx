066982c45d0cfcc3ccc7cb584321721e9a9dbeff
Marco Sosio <marcososio007@gmail.com>
2025-09-04 14:40:30 +0200

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 
 function App() {
-    return <div className="p-1 text-2xl decoration-amber-100"></div>;
+    return(
+        <>
+        </>
+    );
 }
 
 export default App;
